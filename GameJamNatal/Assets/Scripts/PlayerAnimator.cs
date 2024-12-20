682242a5d08dfa49279844c4ccff6214eae37f8b
Leonardo <leo_santos1304@hotmail.com>
2024-12-19 22:45:31 -0300

    ImportacaoCodigo: Momentum
    Implementação dos códigos de movimentação com o momentum. Além disso criei um scriptable object para armazenar os dados de movimentação e momentum.

---
+++ GameJamNatal/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private PlayerMomentum mov;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    [Header("Movement Tilt")]
+    [SerializeField] private float maxTilt;
+    [SerializeField][Range(0, 1)] private float tiltSpeed;
+
+    public bool startedJumping { private get;  set; }
+    public bool justLanded { private get;  set; }
+
+    public float currentVelY;
+
+    private void Start()
+    {
+        mov = GetComponent<PlayerMomentum>();
+        spriteRend = GetComponent<SpriteRenderer>();
+        anim = spriteRend.GetComponent<Animator>();
+    }
+
+    private void LateUpdate()
+    {
+        float tiltProgress;
+
+        int mult = -1;
+
+        if (mov.IsSliding)
+        {
+            tiltProgress = 0.25f;
+        }
+        else
+        {
+            tiltProgress = Mathf.InverseLerp(-mov.Data.runMaxSpeed, mov.Data.runMaxSpeed, mov.rb.velocity.x);
+            mult = (mov.IsFacingRight) ? 1 : -1;
+        }
+
+        float newRot = ((tiltProgress * maxTilt * 2) - maxTilt);
+        float rot = Mathf.LerpAngle(spriteRend.transform.localRotation.eulerAngles.z * mult, newRot, tiltSpeed);
+        spriteRend.transform.localRotation = Quaternion.Euler(0, 0, rot * mult);
+
+        CheckAnimationState();
+    }
+
+    private void CheckAnimationState()
+    {
+        if (startedJumping)
+        {
+            anim.SetTrigger("PlayerJumping");
+            justLanded = false;
+            return;
+        }
+
+        if (justLanded)
+        {
+            anim.SetTrigger("PlayerIdle");
+            justLanded = false;
+            return;
+        }
+
+        anim.SetFloat("Vel Y", mov.rb.velocity.y);
+    }
+}
---
+++ GameJamNatal/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player Data")]
+public class PlayerData : ScriptableObject
+{
+    [Header("Gravity")]
+    [HideInInspector] public float gravityStrenght;
+    [HideInInspector] public float gravityScale;
+
+    [Space(5)]
+    public float fallGravityMult;
+    public float maxFallSpeed;
+
+    [Space(5)]
+    public float fastFallGravityMult;
+    public float maxFastFallSpeed;
+
+    [Space(20)]
+
+    [Header("Run")]
+    public float runMaxSpeed;
+    public float runAcceleration;
+    [HideInInspector] public float runAccelAmount;
+    public float runDecceleration;
+    [HideInInspector] public float runDeccelAmount;
+
+    [Space(5)]
+    [Range(0f, 1)] public float accelInAir;
+    [Range(0f, 1)] public float deccelInAir;
+
+    [Space(5)]
+    public bool doConserveMomentum = true;
+
+    [Space(20)]
+
+    [Header("Jump")]
+    public float jumpHeight;
+    public float jumpTimeToApex;
+    [HideInInspector] public float jumpForce;
+
+    [Header("Both Jumps")]
+    public float jumpCutGravityMult;
+    [Range(0f, 1)] public float jumpHangGravityMult;
+    public float jumpHangTimeThresold;
+    [Space(0.5f)]
+    public float jumpHangAccelerationMult;
+    public float jumpHangMaxSpeedMult;
+
+    [Space(20)]
+
+    [Header("Slide")]
+    public float slideSpeed;
+    public float slideAccel;
+
+    [Header("Assists")]
+    [Range(0.01f, 0.5f)] public float coyoteTime;
+    [Range(0.01f, 0.5f)] public float jumpInputBufferTime;
+
+    private void OnValidate()
+    {
+        gravityStrenght = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
+        gravityScale = gravityStrenght / Physics2D.gravity.y;
+
+        runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
+        runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
+
+        jumpForce = Mathf.Abs(gravityStrenght) * jumpTimeToApex;
+
+        runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
+        runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, runMaxSpeed);
+    }
+}
